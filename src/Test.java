0a28a474802730966614daa4198d05038dcdfa70
nicolabricot <github@nicolabricot.com>
2012-11-21 15:06:25 +0100

    update Test

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,7 @@
+import ground.Season;
 import view.Console;
 
+@SuppressWarnings("unused")
 
 public class Test {
 
@@ -10,13 +12,14 @@ public class Test {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		
-		/*
-		//
+		/**
 		Season s = new Season("http://www.imdb.fr/title/tt0460649/episodes?season=1");
-		Season s = new Season("http://www.imdb.fr/title/tt0364845/episodes?season=2");
+		//Season s = new Season("http://www.imdb.fr/title/tt0364845/episodes?season=2");
 		//System.out.println(s);
-		s.similitude(s.getEpisode(1));
-		*/
+		s.displaySimilitude(s.getEpisode(1));
+		
+		
+		**/
 		
 		new Console().play();
 		
