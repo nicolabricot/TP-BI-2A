b3e47ffaab8b532f9a37a3ed70a484d298ff8d2d
nicolabricot <github@nicolabricot.com>
2012-11-21 18:00:32 +0100

    Main Class

---
+++ src/BusinessIntelligence.java
@@ -0,0 +1,21 @@
+import view.Console;
+
+
+public class BusinessIntelligence {
+
+	public BusinessIntelligence() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		new Console().play();
+		
+	}
+
+}
