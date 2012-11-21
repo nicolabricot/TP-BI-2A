8e04eb2c969dbe02931ea953681e1b72028c0f9d
nicolabricot <github@nicolabricot.com>
2012-11-21 15:06:11 +0100

    Add method to sort the list of similitude

---
+++ src/ground/Couple.java
@@ -0,0 +1,13 @@
+package ground;
+
+public class Couple {
+	
+	public int number;
+	public double similitude;
+	
+	public Couple(int number, double similitude) {
+		this.number = number;
+		this.similitude = similitude;
+	}
+
+}
